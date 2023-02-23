6d04253404914a0d3c5573dcdb6505ca9756405c
Kalyan Dutia <kalyan.dutia@gmail.com>
2023-02-23 17:51:12 +0300

    run black on tests

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 @app.get("/health")
 async def get_health():
     """
-    Get application health. 
-    
+    Get application health.
+
     TODO: fill in implementation with real health check.
     """
     return {"status": "OK"}
