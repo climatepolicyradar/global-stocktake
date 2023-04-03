545cfee3f91191d7daa21a7c6b1514d3687033f9
Kalyan Dutia <kalyan.dutia@gmail.com>
2023-04-03 16:41:12 +0100

    quick /search api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+from typing import Sequence
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv, find_dotenv
+from src.opensearch.client import get_opensearch_client
 
 app = FastAPI()
+
+load_dotenv(find_dotenv())
+opns = get_opensearch_client()
 
 
 @app.get("/health")
@@ -11,3 +19,32 @@ async def get_health():
     TODO: fill in implementation with real health check.
     """
     return {"status": "OK"}
+
+
+class SearchRequest(BaseModel):
+    """Request body for search endpoint."""
+
+    text: str
+    span_types: Sequence[str] = []
+    index: str = "global-stocktake"
+    limit: int = 100
+
+
+@app.post("/search")
+async def search(request: SearchRequest):
+    """Get search results."""
+
+    query_body = {
+        "query": {
+            "bool": {
+                "must": [{"match": {"text": request.text}}],
+            }
+        }
+    }
+
+    if request.span_types:
+        query_body["query"]["bool"].update(
+            {"filter": [{"terms": {"span_types.keyword": request.span_types}}]}  # type: ignore
+        )
+
+    return opns.search(index=request.index, body=query_body)
