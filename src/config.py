31e148eaf0c3ee88b419fc0f4a69fcc53e3877a6
Kalyan Dutia <kalyan.dutia@gmail.com>
2023-05-25 14:49:53 +0100

    remove CONCEPTS_TO_INDEX config from environment variables

--- src/config.py
+++ src/config.py
@@ -1,6 +1,22 @@
-import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-CONCEPTS_TO_INDEX: list[str] = os.getenv("CONCEPTS_TO_INDEX", "").split(",")
+CONCEPTS_TO_INDEX: set[str] = {
+    "technologies",
+    "renewables",
+    "fossil-fuels",
+    "vulnerable-groups",
+    "challenges-and-opportunities",
+    "financial-flows",
+    "climate-related-hazards",
+    "equity-and-justice",
+    "deforestation",
+    "greenhouse-gases",
+    "mitigation",
+    "adaptation",
+    "loss-and-damage",
+    "sectors",
+    "policy-instruments",
+}
+FULL_SPAN_CONCEPTS: set[str] = {"sectors", "policy-instruments"}
