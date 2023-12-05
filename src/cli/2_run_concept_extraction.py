d2f05a2a894467c6b6b0d801e11d108e83869791
Kalyan Dutia <kalyan.dutia@gmail.com>
2023-12-05 20:01:42 +0000

    add split spans csv code to cli

--- src/cli/2_run_concept_extraction.py
+++ src/cli/2_run_concept_extraction.py
@@ -5,8 +5,10 @@ from datetime import datetime
 import click
 from explorer.main import run_explorer
 from classifiers.run_on_full_dataset import run_on_full_dataset
+import pandas as pd
 
 from src.cli.utils import get_logger
+from src.data.split_spans_csvs import MAX_SPANS_PER_FILE
 
 LOGGER = get_logger(__name__)
 
@@ -76,7 +78,6 @@ def main(docs_dir: Path, spans_csv_filename: Optional[str] = None):
             use_transformer=True,
         )
 
-    # TODO: classifier concepts
     for concept, wandb_artifact_name in CLASSIFIER_CONCEPTS.items():
         run_on_full_dataset(
             wandb_artifact_name=wandb_artifact_name,
@@ -84,6 +85,33 @@ def main(docs_dir: Path, spans_csv_filename: Optional[str] = None):
             spans_csv_filename=spans_csv_filename,
             extra_output=True,
         )
+
+    LOGGER.info("Splitting large spans CSV files...")
+    for concept in (
+        EXPLORER_CONCEPTS_ML
+        + EXPLORER_CONCEPTS_NON_ML
+        + list(CLASSIFIER_CONCEPTS.keys())
+    ):
+        spans_csv_path = Path(f"./concepts/{concept}/{spans_csv_filename}")
+        if not spans_csv_path.exists():
+            LOGGER.info(f"No {spans_csv_path} file found. Skipping.")
+            continue
+
+        spans_df = pd.read_csv(spans_csv_path)
+        if len(spans_df) > MAX_SPANS_PER_FILE:
+            LOGGER.info(f"Splitting {concept} spans CSV file into multiple files.")
+            split_dfs = [
+                spans_df[i : i + MAX_SPANS_PER_FILE]
+                for i in range(0, len(spans_df), MAX_SPANS_PER_FILE)
+            ]
+
+            for idx, df in enumerate(split_dfs):
+                df.to_csv(
+                    spans_csv_path.parent / f"{spans_csv_path.stem}_{idx}.csv",
+                    index=False,
+                )
+
+            spans_csv_path.unlink()
 
 
 if __name__ == "__main__":
